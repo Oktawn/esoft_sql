376e4db353f953da09e3d63ec1e2bf682f19bfec
oktawn <belyhmitia@gmail.com>
2024-06-02 23:01:38 +0500

    da

--- task_4.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM Enrollments
-WHERE student_id = 2
-    AND course_id = 3;
-    
-DELETE FROM Courses
-WHERE course_id NOT IN (
-        SELECT course_id
-        FROM Enrollments
-    );
